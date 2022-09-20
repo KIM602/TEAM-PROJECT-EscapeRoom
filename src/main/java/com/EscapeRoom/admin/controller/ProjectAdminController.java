86c4a485cd32f6b718227cf0d6070db616bd195b
socom159 <socom159@gmail.com>
2022-09-20 17:08:05 +0900

    커밋

--- src/main/java/com/EscapeRoom/admin/controller/ProjectAdminController.java
+++ src/main/java/com/EscapeRoom/admin/controller/ProjectAdminController.java
@@ -173,13 +173,8 @@ public class ProjectAdminController {
 		MultipartFile mfN = mrequest.getFile("mImageNormal");
 		MultipartFile mfE = mrequest.getFile("mImageEvent");
 		
-<<<<<<< HEAD
 		String pathA = "D:/CHOISUNKYU/GitHub/skcLocal/EscapeRoom/src/main/webapp/resources/upimage/";
 		String pathB = "C:/project/server/apache-tomcat-9.0.65/wtpwebapps/EscapeRoom_security/resources/upimage/";	
-=======
-		String pathA = "D:/osm/workSpace/EscapeRoom_security/src/main/webapp/resources/upimage/";
-		String pathB = "C:/kook/apache-tomcat-9.0.63/wtpwebapps/EscapeRoom_security/resources/upimage/";	
->>>>>>> refs/heads/master
 		
 		String orignalFileNameL = mfL.getOriginalFilename();
 		String orignalFileNameN = mfN.getOriginalFilename();
